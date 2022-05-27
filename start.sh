7114dd185d67cf612e16f0f431b35c8fb79c7980
Ariestechy <90320056+Ariestechy@users.noreply.github.com>
2022-05-27 19:34:20 +0530

    start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone hhttps://github.com/Ariestechy/meowwwww.git /Ajax
+  git clone hhttps://github.com/Ariestechy/meowwwww.git /x
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Ajax
