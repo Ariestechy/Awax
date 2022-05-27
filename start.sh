8a4502413dcf03dd63d64e7a195b19e7c379b0dc
Ariestechy <90320056+Ariestechy@users.noreply.github.com>
2022-05-27 19:36:18 +0530

    start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone hhttps://github.com/Ariestechy/meowwwww.git /x
+  git clone hhttps://github.com/Ariestechy/Awax.git /Awax
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Ajax
+  git clone $UPSTREAM_REPO /Awax
 fi
-cd /Ajax
+cd /Awax
 pip3 install -U -r requirements.txt
 echo "Starting 𝞚𝙥𝙥𝙡𝙚✘....🔥"
 python3 bot.py
